2fd7da961de3a7a5d2f958dbc720bffda0df2bd5
marv's Mac <marvelmd@gmail.com>
2024-05-15 18:53:18 +0300

    fix: remove unnecessary content and update .gitignore

---
+++ src/test/java/org/example/HangmanTest.java
@@ -0,0 +1,41 @@
+package org.example;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class HangmanTest {
+    private Hangman hangman;
+
+    @Before
+    public void setUp() {
+        hangman = new Hangman("hangman", 10);
+    }
+
+    @Test
+    public void guessLetterCorrectlyUpdatesHiddenWord() {
+        hangman.guess('h');
+        assertEquals("h______", hangman.getHiddenWord());
+    }
+
+    @Test
+    public void guessLetterIncorrectlyKeepsHiddenWordSame() {
+        hangman.guess('z');
+        assertEquals("_______", hangman.getHiddenWord());
+    }
+
+    @Test
+    public void guessLetterCorrectlyReturnsTrue() {
+        assertTrue(hangman.guess('h'));
+    }
+
+    @Test
+    public void guessLetterIncorrectlyReturnsFalse() {
+        assertFalse(hangman.guess('z'));
+    }
+
+    @Test
+    public void isGameOverReturnsFalseInitially() {
+        assertFalse(hangman.isGameOver());
+    }
+}
